985c7e0a6970f75fc5b891bfbee9097ab3376cc2
Hamza <Hamza5955>
2021-09-06 17:05:43 +0200

    fine

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+Buon lavoro... ci vediamo mercoledì :slightly_smiling_face:*/
+
+Vue.config.devtools = true;
+
+const app = new Vue({
+  el: "#root",
+  data: {
+    message: "hello world !",
+  },
+});
